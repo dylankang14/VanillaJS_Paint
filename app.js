56bcb5cfb3a142f8c539b55a7888cb271917cda4
dylan <dylankang14@gmail.com>
2022-03-07 17:29:52 +0900

    feat: changing brush size

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const canvas = document.getElementById('jsCanvas');
 const ctx = canvas.getContext("2d");
 const colors = document.querySelectorAll('.control__color');
+const range = document.querySelector('#jsRange');
+const modeBtn = document.querySelector('#jsMode');
 
 canvas.width = 700;
 canvas.height = 700;
@@ -9,6 +11,7 @@ ctx.strokeStyle = '#2c2c2c';
 ctx.lineWidth = 2.5;
 
 let painting = false;
+let filling = false;
 
 function stopPaint() {
     painting = false;
@@ -35,6 +38,20 @@ function colorHandler(event) {
     ctx.strokeStyle = event.target.style.backgroundColor
 }
 
+function rangeHandler(event) {
+    ctx.lineWidth = event.target.value;
+}
+
+function modeHandler(event) {
+    if (filling === true) {
+        filling = false;
+        modeBtn.innerText = 'fill';
+    } else {
+        filling = true;
+        modeBtn.innerText = 'paint';
+    }
+}
+
 if (canvas) {
     canvas.addEventListener('mousemove', onMouseMove);
     canvas.addEventListener('mousedown', startPaint);
@@ -43,3 +60,11 @@ if (canvas) {
 }
 
 colors.forEach(color => color.addEventListener('click', colorHandler));
+
+if (range) {
+    range.addEventListener('input', rangeHandler);
+}
+
+if (modeBtn) {
+    modeBtn.addEventListener('click', modeHandler);
+}
